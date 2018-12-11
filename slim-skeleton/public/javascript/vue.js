2e32f4d5164a2e8c9e15326915a83909fa5ad510
Drees Camertijn <dreescamertijn@hotmail.com>
2018-12-11 09:45:50 +0100

    opslaan op server werkt

--- slim-skeleton/public/javascript/vue.js
+++ slim-skeleton/public/javascript/vue.js
@@ -31,3 +31,54 @@ var vm = new Vue({
     }
 })
 
+// canvas tekenen pixels
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+        
+document.getElementById("button").onclick = function(){            
+var userColor = document.getElementById("colorselect").value;
+var userpixx = document.getElementById("userpixx").value;
+var userpixy = document.getElementById("userpixy").value;
+ctx.fillStyle = userColor;
+ctx.fillRect(userpixx, userpixy, 10, 10);
+};
+
+//canvas to json 
+
+var reader = new FileReader();
+
+// event handler for the save button
+document.getElementById('button').addEventListener('click', function () {
+  // retrieve the canvas data
+  var canvasContents = canvas.toDataURL(); // a data URL of the current canvas image
+  var data = { image: canvasContents, date: Date.now() };
+  var string = JSON.stringify(data);
+
+  // 
+fetch('/submit',{
+method: 'POST',
+body: string,
+headers: { "Content-Type": "application/json; charset=utf-8" }
+})
+
+  
+});
+
+// event handler for the load button
+document.getElementById('load').addEventListener('change', function () {
+  if (this.files[0]) {
+    // read the contents of the first file in the <input type="file">
+    reader.readAsText(this.files[0]);
+  }
+});
+
+// this function executes when the contents of the file have been fetched
+reader.onload = function () {
+  var data = JSON.parse(reader.result);
+  var image = new Image();
+  image.onload = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, 0, 0); // draw the new image to the screen
+  }
+  image.src = data.image; // data.image contains the data URL
+};
--- slim-skeleton/src/routes.php
+++ slim-skeleton/src/routes.php
@@ -11,4 +11,22 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+
+});
+
+$app->post('/submit', function (Request $request, Response $response, array $args) {
+   
+    $body = $request->getParsedBody();
+
+    $myfile = fopen("newfile.png", "w") or die("Unable to open file!");
+
+    $data = explode( ',', $body['image'] );
+
+    // we could add validation here with ensuring count( $data ) > 1
+    fwrite($myfile, base64_decode( $data[ 1 ] ) );
+    fclose($myfile);
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+
 });
